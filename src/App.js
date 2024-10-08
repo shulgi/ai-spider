2f370e74d77db2f506d1db87d89ef424a6d62b7f
ronen raz <journey@blixa.org>
2024-10-07 23:07:32 -0700

    change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Winery Information Dashboard</h1>
+        <h1>The Winery Information Dashboard</h1>
       </header>
       <main>
         <WineryInfoFetcher />
